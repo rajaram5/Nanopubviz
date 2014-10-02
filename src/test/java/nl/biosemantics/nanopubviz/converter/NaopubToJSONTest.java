4840091ea6823f9fcc8d5e2303c00a97fd1d5c6c
rr.kaliyaperumal@gmail.com <whoisnext5>
2014-10-02 12:35:13 +0200

    some unit test

--- src/test/java/nl/biosemantics/nanopubviz/converter/NaopubToJSONTest.java
+++ src/test/java/nl/biosemantics/nanopubviz/converter/NaopubToJSONTest.java
@@ -1,14 +1,11 @@
+package nl.biosemantics.nanopubviz.converter;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 import java.io.IOException;
-import nl.biosemantics.nanopubviz.converter.NanopubToJSON;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.nanopub.MalformedNanopubException;
@@ -33,5 +30,19 @@ public class NaopubToJSONTest {
         NanopubToJSON test = new NanopubToJSON(npURI, RDFFormat.NQUADS);
         
         assertNotNull(test);
-    } 
+    }
+    
+    @Test(expected=MalformedNanopubException.class)
+    public void invalidNanopub() throws Exception {
+        String npURI = "http://rdf.biosemantics.org/nanopubs/cpm/protein_protein_associations/000001"; 
+        
+        NanopubToJSON test = new NanopubToJSON(npURI, RDFFormat.NQUADS);
+    }
+    
+    @Test(expected=IOException.class)
+    public void invalidNanopubURL() throws Exception {
+        String npURI = "http://rdf.biosemantics.org/nanopubs/000001-invalid"; 
+        
+        NanopubToJSON test = new NanopubToJSON(npURI, RDFFormat.NQUADS);
+    }
 }
